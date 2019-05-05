4d3d055b475e90ef64bf687ef34a8306a76e08c7
Thomas Malt <thomas@malt.no>
2019-05-06 01:07:14 +0200

    Creating a couple of helpers with default data

---
+++ src/utils/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  white: '#FFFFFF',
+  dust_storm: '#D8CBC7',
+  silver_lake_blue: '#6193BF',
+  cosmic_latte: '#FFFBE5',
+  azureish: '#D4E6FC',
+}
+
+export default colors
---
+++ src/utils/settings.js
@@ -0,0 +1,9 @@
+const settings = {
+  urls: {
+    token: 'https://api.malt.no/banking/oauth2/token',
+    balance: 'https://api.malt.no/banking/balance',
+  },
+}
+
+export default settings
+export const urls = settings.urls

5779547b945f7027442e56ace89cebb4d1cdc0bb
Thomas Malt <thomas@malt.no>
2019-05-08 01:34:48 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Saldo
-
-This is a simple app using my banks open api's to provide account information to my kids.
--- src/utils/settings.js
+++
@@ -1,9 +0,0 @@
-const settings = {
-  urls: {
-    token: 'https://api.malt.no/banking/oauth2/token',
-    balance: 'https://api.malt.no/banking/balance',
-  },
-}
-
-export default settings
-export const urls = settings.urls

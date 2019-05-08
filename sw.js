a632c2119d528c9aee311b8a062a12d5254726e6
Thomas Malt <thomas@malt.no>
2019-05-08 02:02:29 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-90632885e24abd1ee774.js"
+    "url": "webpack-runtime-5956fbdfdcd0130e4cfe.js"
   },
   {
     "url": "app-7363431141508cdd22d4.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cff8c0685e64296d0b6a077c544390a6"
+    "revision": "0b8ba21d99eb29b122ff6b7388c9522c"
   },
   {
     "url": "styles.af1e3cbceb74626e6301.css"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "1-daeae648a2f429e095fe.js"
   },
   {
-    "url": "component---src-pages-404-js-ee0096b783aa207bd1ea.js"
+    "url": "component---src-pages-404-js-f44373d6e519816fdc2c.js"
   },
   {
     "url": "static/d/285/path---404-html-516-62a-0SUcWyAf8ecbYDsMhQkEfPzV8.json"

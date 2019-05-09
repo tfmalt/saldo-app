556502cb1b88a84857b0cbab8b7414828cb05bdf
Thomas Malt <thomas@malt.no>
2019-05-09 23:41:33 +0200

    started using the google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['material icons', 'sigmar one', 'roboto:300,400,500,700'],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {

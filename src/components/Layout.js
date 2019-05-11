8b23e09401cd44a0f88788e8b5df97a33bf7ced8
Thomas Malt <thomas@malt.no>
2019-05-11 18:53:47 +0200

    cleaning up code and moving to sass

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,44 +10,21 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Header from '../components/Header'
 import { version } from '../../package'
-import { themes } from '../utils/colors'
 import './layout.css'
+import './Layout.module.scss'
 
-const theme = themes.teal
-
-const Footer = styled.footer`
-  font-family: 'Roboto', sans-serif;
-  background-color: ${theme.primary.main};
-  padding: 0.5rem;
-  text-align: center;
-  color: #404040;
-  font-size: 0.9em;
-  position: fixed;
-  width: 100%;
-  bottom: 0px;
-  box-shadow: 0px -2px 10px rgba(0, 0, 0, 0.3);
-  div#built-with {
-    font-size: 0.8em;
-  }
-`
 const Version = styled.span`
   font-weight: 700;
-`
-
-const Main = styled.main`
-  padding: 14vh 6vw;
-  background-color: ${theme.secondary.main};
-  height: 100%;
 `
 
 class Layout extends React.Component {
   render() {
     const { headerTitle, children } = this.props
     return (
-      <>
+      <main>
         <Header headerTitle={headerTitle} />
-        <Main>{children}</Main>
-        <Footer>
+        <section>{children}</section>
+        <footer>
           <div>
             Version <Version>{version}</Version> by Thomas Malt ©{' '}
             {new Date().getFullYear()}
@@ -57,8 +34,8 @@ class Layout extends React.Component {
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </div>
-        </Footer>
-      </>
+        </footer>
+      </main>
     )
   }
 }

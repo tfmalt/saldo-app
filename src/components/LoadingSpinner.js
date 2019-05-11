98c4ca7195ace4569abd748414145404e099b6f6
Thomas Malt <thomas@malt.no>
2019-05-11 18:54:47 +0200

    started work on a material design progress spinner

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import css from './LoadingSpinner.module.scss'
+
+export default () => (
+  <div className={css.wrapper}>
+    <div className={css.spinner} />
+  </div>
+)

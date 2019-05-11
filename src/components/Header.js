9ed3d2f482b1331ea82bb069df4c32f4e3c7188c
Thomas Malt <thomas@malt.no>
2019-05-11 18:52:55 +0200

    cleaning up code and moving to sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 // import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
-import { themes } from '../utils/colors'
+// import styled from 'styled-components'
+// import { themes } from '../utils/colors'
 import Icon from './Icon'
+import AppBar from './AppBar'
+import css from './Header.module.scss'
 
-const theme = themes.teal
+// const theme = themes.teal
 
 // <Title>{headerTitle}</Title>
 class Header extends React.Component {
@@ -14,7 +16,7 @@ class Header extends React.Component {
 
     console.log('header constructor')
     this.state = {
-      mainMenu: 'main-menu-closed',
+      mainMenu: css.mainMenuClosed,
     }
 
     this.openMenu = this.openMenu.bind(this)
@@ -22,19 +24,43 @@ class Header extends React.Component {
 
   openMenu = event => {
     console.log('open menu:', event)
+    this.setState({
+      mainMenu: css.mainMenuOpen,
+    })
+  }
+
+  closeMenu = event => {
+    console.log('close menu:', event)
+    this.setState({
+      mainMenu: css.mainMenuClosed,
+    })
+  }
+  handleTouchStart = event => {
+    console.log('touch start:', event)
   }
 
   render() {
     const { headerTitle } = this.props
     return (
       <>
-        <MyHeader>
-          <div>
-            <Icon name="menu" trigger={this.openMenu} />
-            <Title>{headerTitle}</Title>
+        <header>
+          <AppBar headerTitle={headerTitle} trigger={this.openMenu} />
+        </header>
+        <nav
+          id={css.mainMenu}
+          onTouchStart={this.handleTouchStart}
+          className={this.state.mainMenu}
+        >
+          <div id="TopBar">
+            <h6 className={css.topbarTitle}>Hovedmeny</h6>
+            <Icon name="close" trigger={this.closeMenu} />
           </div>
-        </MyHeader>
-        <MainMenu className={this.state.mainMenu}>Hello</MainMenu>
+          <div className={css.topbarMenuItems}>
+            <ul>
+              <li>Logg ut</li>
+            </ul>
+          </div>
+        </nav>
       </>
     )
   }
@@ -49,38 +75,3 @@ Header.defaultProps = {
 }
 
 export default Header
-
-const MyHeader = styled.header`
-  background-color: ${theme.primary.main};
-  color: black;
-  top: 0px;
-  width: 100%;
-  height: 72px;
-  position: fixed;
-
-  div {
-    display: flex;
-    flex-direction: row;
-    padding: 15px 12px;
-  }
-
-  & > * {
-    margin-right: 12px;
-  }
-`
-
-const Title = styled.h1`
-  text-decoration: none;
-  font-size: 1.2em;
-  display: inline-block;
-  padding-top: 8px;
-  padding-left: 32px;
-`
-
-const MainMenu = styled.div`
-  position: absolute;
-  z-index: 100;
-  top: 0px;
-  height: 100vh;
-  background-color: ${theme.secondary.dark};
-`

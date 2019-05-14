808ac48508bd3f9d388cfdab3d4c764f2a3000ca
Thomas Malt <thomas@malt.no>
2019-05-15 00:27:10 +0200

    Implementing log out

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,32 @@ import React from 'react'
 import Icon from './Icon'
 import AppBar from './AppBar'
 import css from './Header.module.scss'
+import localforage from 'localforage'
+import { navigate } from 'gatsby'
 
 class Header extends React.Component {
   constructor(props) {
     super(props)
 
+    localforage.config({ name: 'saldo' })
+
     console.log('header constructor')
     this.state = {
       mainMenu: css.mainMenuClosed,
+      userId: 'Ukjent',
     }
 
     this.openMenu = this.openMenu.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
   }
 
-  openMenu = event => {
-    console.log('open menu:', event)
-    this.setState({
-      mainMenu: css.mainMenuOpen,
-    })
-  }
+  async componentDidMount() {
+    const { userId } = (await localforage.getItem('credentials')) || {
+      userId: 'Ukjent',
+    }
+    console.log('Header did mount: userId:', userId)
 
-  closeMenu = event => {
-    console.log('close menu:', event)
-    this.setState({
-      mainMenu: css.mainMenuClosed,
-    })
-  }
-  handleTouchStart = event => {
-    console.log('touch start:', event)
+    this.setState({ userId })
   }
 
   render() {
@@ -51,12 +49,48 @@ class Header extends React.Component {
           </div>
           <div className={css.topbarMenuItems}>
             <ul>
-              <li>Logg ut</li>
+              <li>
+                <div>
+                  <span>Logget inn som:</span>{' '}
+                  <span className={css.userId}>{this.state.userId}</span>
+                </div>
+                <button
+                  onClick={this.handleLogout}
+                  className={css.logoutButton}
+                >
+                  Logg ut
+                </button>
+              </li>
             </ul>
           </div>
         </nav>
       </>
     )
+  }
+
+  async handleLogout() {
+    console.log('Got logout')
+
+    // we clean the database then.
+    await localforage.clear()
+    return navigate('/authenticate')
+  }
+
+  openMenu = event => {
+    console.log('open menu:', event)
+    this.setState({
+      mainMenu: css.mainMenuOpen,
+    })
+  }
+
+  closeMenu = event => {
+    console.log('close menu:', event)
+    this.setState({
+      mainMenu: css.mainMenuClosed,
+    })
+  }
+  handleTouchStart = event => {
+    console.log('touch start:', event)
   }
 }
 
